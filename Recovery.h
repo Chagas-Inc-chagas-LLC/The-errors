cb8b24d4211e5c462739d7eb75e966f213bc0c15
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-11-10 11:06:25 -0300

    Create Recovery.h

---
+++ Recovery.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void recovery_main() {
+  prinf("==> Reboot");
+printf("==> view recovery logs");
+  printf("wipe cache/");
+printf("update to sdcard");
+printf("factory reset");
+}
