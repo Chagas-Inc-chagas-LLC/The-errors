41a0cbfc24928342818d811bf8299cb58f331c29
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-11-10 11:14:11 -0300

    Create FactoryReset.h

---
+++ FactoryReset.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void FactoryReset() {
+  printf("FACTORY RESET");
+printf("WARNING:The factory reset may erase your data. Do you want to make a backup? Or not?");
+printf("YES OR NO");
+printf("==> no");
+printf("==> no");
+printf("==> yes")
+}
